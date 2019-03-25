15b5b269476ce4166f65aef5fee589966bd93107
Jimmy McDermott <mcdappdev@gmail.com>
2019-03-25 14:47:11 -0400

    Resource

--- Sources/OneRoster/Models/Org.swift
+++ Sources/OneRoster/Models/Org.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+/// ORG is defined here as a structure for holding organizational information.
+/// An ORG might be a school, or it might be a local, statewide, or national entity.
+/// ORGs will typically have a parent ORG (up to the national level), and children,
+/// allowing a hierarchy to be established.
+///
+/// School is defined here as the place where the learning happens. Most commonly this
+/// is the data that describes a bricks and mortar building, or, in the case of a virtual school,
+/// the virtual school organization. For enrollment and result reporting purposes, little information
+/// about this organization is required. Later versions of the specification could add further information,
+/// such as an address, for example. A common example of a local organization is a school district.
+///
+/// Note that although School is a type of org, the default entry point for requests in most places will be a school.
+/// The API provides many school based entry points, whilst still allowing for more generic reading of ORGs,
+/// for those applications that need to.
 public struct Org: Codable, OneRosterBase {
     /// See `OneRosterBase.sourceId`
     public var sourcedId: String
--- Sources/OneRoster/Models/Resource.swift
+++ Sources/OneRoster/Models/Resource.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+/// A resource is a description of learning content that is related to a course and/or a class.
+/// This identifies a resource that is used by a teacher, learner, etc.
+/// as part of the learning experience. A resource MUST be associated to a course and/or a class.
 public struct Resource: Codable, OneRosterBase {
     /// See `OneRosterBase.sourceId`
     public var sourcedId: String
@@ -19,4 +22,47 @@ public struct Resource: Codable, OneRosterBase {
     
     /// See `OneRosterBase.metadata`
     public var metadata: [String: String]?
+    
+    /// For example: Organic Chemistry
+    public var title: String?
+    
+    /// The set of roles. See subsection 4.13.5 for the enumeration list.
+    public var roles: [RoleType]?
+    
+    /// See subsection 4.13.3 or `Importance` for the enumeration list.
+    public var importance: Importance?
+    
+    /// Unique identifier for the resource allocated by the vendor.
+    public var vendorResourceId: String
+    
+    /// Identifier for the vendor who created the resource.
+    /// This will be assigned by IMS as part of Conformance Certification.
+    public var vendorId: String?
+    
+    /// Identifier for the application associated with the resource.
+    public var applicationId: String?
+
+    /// Create a new `Resource`
+    public init(sourcedId: String,
+                status: StatusType,
+                dateLastModified: Date,
+                metadata: [String: String]?,
+                title: String?,
+                roles: [RoleType]?,
+                importance: Importance?,
+                vendorResourceId: String,
+                vendorId: String?,
+                applicationId: String?)
+    {
+        self.sourcedId = sourcedId
+        self.status = status
+        self.dateLastModified = dateLastModified
+        self.metadata = metadata
+        self.title = title
+        self.roles = roles
+        self.importance = importance
+        self.vendorResourceId = vendorResourceId
+        self.vendorId = vendorId
+        self.applicationId = applicationId
+    }
 }
