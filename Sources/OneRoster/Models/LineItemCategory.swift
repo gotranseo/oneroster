8e6d2dae314947ede33609211c447efcf557ce2d
Jimmy McDermott <mcdappdev@gmail.com>
2019-03-25 14:39:44 -0400

    line item category

--- Sources/OneRoster/Models/LineItemCategory.swift
+++ Sources/OneRoster/Models/LineItemCategory.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+/// A Category is the name given to a grouping of line items.
+/// (Line Items being equivalent to assignments which students will complete).
+///
+/// Examples of categories include "homework", "quizzes" or "essays". It is proposed that
+/// the Category object be defined as shown in Figure 4.9/Table 4.8.
 public struct LineItemCategory: Codable, OneRosterBase {
     /// See `OneRosterBase.sourceId`
     public var sourcedId: String
@@ -19,4 +24,21 @@ public struct LineItemCategory: Codable, OneRosterBase {
     
     /// See `OneRosterBase.metadata`
     public var metadata: [String: String]?
+    
+    /// For example: Homework
+    public var title: String
+
+    /// Create a new `LineItemCategory`
+    public init(sourcedId: String,
+                status: StatusType,
+                dateLastModified: Date,
+                metadata: [String: String]?,
+                title: String)
+    {
+        self.sourcedId = sourcedId
+        self.status = status
+        self.dateLastModified = dateLastModified
+        self.metadata = metadata
+        self.title = title
+    }
 }
