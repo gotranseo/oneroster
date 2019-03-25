1d28e29064659df3ddd8c53352e9b9a571fc3b7a
Jimmy McDermott <mcdappdev@gmail.com>
2019-03-25 15:17:26 -0400

    Result

--- Sources/OneRoster/Models/Result.swift
+++ Sources/OneRoster/Models/Result.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+/// It is proposed that the OneRoster Result object takes a subset of the
+/// equivalent LIS elements as shown in Figure 4.12/Table 4.11.
 public struct Result: Codable, OneRosterBase {
     /// See `OneRosterBase.sourceId`
     public var sourcedId: String
@@ -19,4 +21,22 @@ public struct Result: Codable, OneRosterBase {
     
     /// See `OneRosterBase.metadata`
     public var metadata: [String: String]?
+    
+    /// Link to lineItem i.e. the lineItem's 'sourcedId'.
+    public var lineItem: GUIDRef
+    
+    /// Link to student i.e. the user's 'sourcedId'.
+    public var student: GUIDRef
+    
+    /// See subsection 4.13.6 or `ScoreStatus` for the enumeration list.
+    public var scoreStatus: ScoreStatus
+    
+    /// For example: 67.0
+    public var score: Double
+    
+    /// For example: 2012-01-05
+    public var scoreDate: Date
+    
+    /// For example: excellent
+    public var comment: String?
 }
