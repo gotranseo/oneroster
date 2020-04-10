ee12e941e68b7f60eb554433fd3912c60cd3f240
Jimmy McDermott <mcdappdev@gmail.com>
2020-04-10 14:47:14 -0500

    Request extension

---
+++ Sources/OneRoster/Client/Request+OneRoster.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jimmy McDermott on 4/10/20.
+//
+
+import Foundation
+import Vapor
+
+extension Application {
+    public var oneRoster: OneRosterClient {
+        return OneRosterClient(client: self.client)
+    }
+}
+
+extension Request {
+    public var oneRoster: OneRosterClient {
+        return OneRosterClient(client: self.client)
+    }
+}
